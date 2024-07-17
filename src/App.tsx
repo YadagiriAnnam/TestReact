6c4eb91f14c54cddd423c21e68593d8f1c895471
Yadagiri Annam <yadagiria437@gmail.com>
2024-07-17 10:17:43 +0000

    Added New

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { PostList } from "./Posts";
 
 function App() {
+  const authProvider = {
+    // authentication
+    login: (params: any) => Promise.resolve(/* ... */),
+    checkError: (error: any) => Promise.resolve(/* ... */),
+    checkAuth: (params: any) => Promise.resolve(/* ... */),
+    logout: () => Promise.resolve(/* ... */),
+    // authorization
+    getPermissions: () => Promise.resolve("admin"),
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Admin
+        dataProvider={jsonServerProvider(
+          "https://jsonplaceholder.typicode.com"
+        )}
+        authProvider={authProvider}
+      >
+        <Resource name="posts" list={PostList}  />
+      </Admin>
     </div>
   );
 }
---
+++ src/Posts.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  DateField,
+  BooleanField,
+  ListProps,
+} from "react-admin";
+
+export const PostList = (props: any) => (
+  <List {...props}>
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="title" />
+      <DateField source="published_at" />
+      <TextField source="category" />
+      <BooleanField source="commentable" />
+    </Datagrid>
+  </List>
+);
